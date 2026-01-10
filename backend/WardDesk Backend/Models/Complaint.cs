116abfd75af0d6d1040209b1d26cdc461a5d0f72
Noble Nepal <nepalnoble525@gmail.com>
2026-01-10 12:46:41 +0545

    Creation of entities of complaint

---
+++ backend/WardDesk Backend/Models/Complaint.cs
@@ -0,0 +1,13 @@
+﻿namespace WardDesk_Backend.Models
+{
+    public class Complaint
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
