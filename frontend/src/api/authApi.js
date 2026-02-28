a5da24df4ce640e3046ce39dba17bfc89023778e
Noble Nepal <nepalnoble525@gmail.com>
2026-02-28 17:09:21 +0545

    Improvement of authentication api naming

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,22 +1,25 @@
 import axiosInstance from "./axiosInstance";
-
+import { API_ENDPOINTS } from "../constants/apiEndpoints";
 export const loginUser = async (email, password) => {
-  const response = await axiosInstance.post("/auth/login", { email, password });
+  const response = await axiosInstance.post(API_ENDPOINTS.LOGIN, {
+    email,
+    password,
+  });
   return response.data;
 };
 
 export const registerUser = async (data) => {
-  const response = await axiosInstance.post("/auth/register", data);
+  const response = await axiosInstance.post(API_ENDPOINTS.REGISTER, data);
   return response.data;
 };
 
 export const logoutUser = async () => {
-  const response = await axiosInstance.post("/auth/logout");
+  const response = await axiosInstance.post(API_ENDPOINTS.LOGOUT);
   return response.data;
 };
 
 export const refreshToken = async (refreshToken) => {
-  const response = await axiosInstance.post("/auth/refresh-token", {
+  const response = await axiosInstance.post(API_ENDPOINTS.REFRESH_TOKEN, {
     refreshToken,
   });
   return response.data;
