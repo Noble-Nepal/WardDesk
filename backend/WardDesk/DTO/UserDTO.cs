7c4ba60f80aca786a881783b500ead9abb0a2f42
Noble Nepal <nepalnoble525@gmail.com>
2026-03-31 12:44:31 +0545

    UserDTO addition for maintaining consitancy in data transfer

---
+++ backend/WardDesk/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace WardDesk.DTO
+{
+    public class UserDTO
+    {
+        public Guid UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int WardNumber { get; set; }
+        public string Role { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsVerified { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
