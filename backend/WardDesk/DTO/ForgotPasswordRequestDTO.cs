b3b58d47bb6db53a432ede1379cff9021f04c4bc
Noble Nepal <nepalnoble525@gmail.com>
2026-04-03 08:04:00 +0545

    Integration of DTO for Forgot password Functionality

---
+++ backend/WardDesk/DTO/ForgotPasswordRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WardDesk.DTO
+{
+    public class ForgotPasswordRequestDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+    }
+}
---
+++ backend/WardDesk/DTO/ResetPassowrdRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WardDesk.DTO
+{
+    public class ResetPasswordRequestDTO
+    {
+        [Required]
+        public string OobCode { get; set; } = "";
+
+        [Required]
+        [MinLength(6)]
+        public string NewPassword { get; set; } = "";
+    }
+}
